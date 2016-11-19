2274c94cada1fb8dec0983b0d92951d6ad4bc064
Rickard Nisses-Gagnér <rickard@nisses-gagner.se>
2016-11-19 11:51:59 +0100

    Added bundling

---
+++ Source/Judoka/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace Judoka
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Content/js/jquery-{version}.js",
+                        "~/Content/js/boostrap.js",
+                        "~/Content/js/judoka.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                        "~/Content/css/bootstrap.css",
+                        "~/Content/css/starter-template.css"));
+        }
+    }
+}
